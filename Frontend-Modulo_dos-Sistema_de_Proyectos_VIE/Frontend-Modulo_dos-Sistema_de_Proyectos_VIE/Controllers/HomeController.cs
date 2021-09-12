1261cd233a0a15bc018300563a574ba592f9e71a
Marlen Solano <marpasoso@hotmail.com>
2021-09-12 15:37:49 -0600

    Error Fixed and View Design

--- Frontend-Modulo_dos-Sistema_de_Proyectos_VIE/Frontend-Modulo_dos-Sistema_de_Proyectos_VIE/Controllers/HomeController.cs
+++ Frontend-Modulo_dos-Sistema_de_Proyectos_VIE/Frontend-Modulo_dos-Sistema_de_Proyectos_VIE/Controllers/HomeController.cs
@@ -15,9 +15,9 @@ namespace Frontend_Modulo_dos_Sistema_de_Proyectos_VIE.Controllers
 
         public ActionResult Index()
         {
-            HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Investigador/4").Result;
-            var investigador = response.Content.ReadAsAsync<oInvestigador>().Result;
-            return View(investigador);
+            /*HttpResponseMessage response = GlobalVariables.WebApiClient.GetAsync("Investigador/4").Result;
+            var investigador = response.Content.ReadAsAsync<oInvestigador>().Result;*/
+            return View("FichaInvestigador");
 
         }
 
