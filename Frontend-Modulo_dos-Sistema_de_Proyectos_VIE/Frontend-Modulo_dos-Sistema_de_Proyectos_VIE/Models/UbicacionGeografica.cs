562c3333e256505917baf090822d85cd8c3e437a
jose <josefabio20@estudiantec.cr>
2021-10-22 12:09:44 -0600

    Ubicacion de nuevo

---
+++ Frontend-Modulo_dos-Sistema_de_Proyectos_VIE/Frontend-Modulo_dos-Sistema_de_Proyectos_VIE/Models/UbicacionGeografica.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Frontend_Modulo_dos_Sistema_de_Proyectos_VIE.Models
+{
+    public class UbicacionGeografica
+    {
+        #region Variables y Constantes
+        [JsonProperty("Id")]
+        public String Id { get; set; }
+        [JsonProperty("IdPais")]
+        public String IdPais { get; set; }
+        [JsonProperty("IdRegion")]
+        public String IdRegion { get; set; }
+        [JsonProperty("IdProvincia")]
+        public String IdProvincia { get; set; }
+        [JsonProperty("CodigoProyecto")]
+        public String CodigoProyecto { get; set; }
+
+
+        #endregion
+    }
+}
