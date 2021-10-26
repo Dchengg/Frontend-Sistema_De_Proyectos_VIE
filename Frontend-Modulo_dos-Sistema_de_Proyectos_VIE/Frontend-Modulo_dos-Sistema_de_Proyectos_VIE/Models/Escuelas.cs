abd3d8798761ab503e8c5777007f4c0360ca28e5
Daar375 <Daar375@gmail.com>
2021-10-26 13:44:49 -0600

    UIVisualizacion

---
+++ Frontend-Modulo_dos-Sistema_de_Proyectos_VIE/Frontend-Modulo_dos-Sistema_de_Proyectos_VIE/Models/Escuelas.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Frontend_Modulo_dos_Sistema_de_Proyectos_VIE.Models
+{
+    public class Escuelas
+    {
+        #region Variables y Constantes
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("NombreEscuela")]
+        public String NombreEscuela { get; set; }
+        [JsonProperty("IdSede")]
+        public int IdSede { get; set; }
+        [JsonProperty("Sede")]
+        public String Sede { get; set; }
+
+        #endregion
+
+    }
+}
